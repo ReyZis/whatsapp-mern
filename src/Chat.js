3509e5f11bbd71fd8efc5950feb6ee2219b286c2
unknown <ramidz130@gmail.com>
2021-08-10 01:20:41 +0100

    Chat component completed (message input bar)

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,8 @@ import { Avatar, IconButton } from "@material-ui/core";
 import React from "react";
 import "./Chat.css";
 import { SearchOutlined, MoreVert, AttachFile } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
     return (
@@ -50,6 +52,17 @@ function Chat() {
                         {new Date().toUTCString()}
                     </span>
                 </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input 
+                        type="text"
+                        placeholder="Type a message"
+                    />
+                    <button type="submit" >Send a message</button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     );
